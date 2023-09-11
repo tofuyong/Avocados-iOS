5159c437ce45587431df7f0a4c414b715fbd85c0
your_name <tofuyong@gmail.com>
2023-09-11 15:49:47 +0800

    completed tab view and avocados view

--- Avocados/Views/AppView.swift
+++ Avocados/Views/AppView.swift
@@ -9,7 +9,30 @@ import SwiftUI
 
 struct AppView: View {
     var body: some View {
-        Text("App View")
+        TabView {
+            AvocadosView()
+                .tabItem ({
+                    Image("tabicon-branch")
+                    Text("Avocados")
+                })
+            ContentView()
+                .tabItem ({
+                    Image("tabicon-book")
+                    Text("Recipes")
+                })
+            RipeningStagesView()
+                .tabItem({
+                    Image("tabicon-avocado")
+                    Text("Ripening")
+                })
+            SettingsView()
+                .tabItem({
+                    Image("tabicon-settings")
+                    Text("Settings")
+                })
+        }
+        .edgesIgnoringSafeArea(.top)
+        .accentColor(Color.primary)
     }
 }
 
--- Avocados/Views/AvocadosView.swift
+++ Avocados/Views/AvocadosView.swift
@@ -8,8 +8,53 @@
 import SwiftUI
 
 struct AvocadosView: View {
+    @State private var pulsateAnimation: Bool = false
+    
     var body: some View {
-        Text("Avocados")
+        VStack{
+            Spacer()
+            Image("avocado")
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .frame(width: 240, height: 240)
+                .shadow(color: Color("ColorBlackTransparentDark"), radius: 12, x:0, y:8)
+                .scaleEffect(self.pulsateAnimation ? 1 : 0.9)
+                .opacity(self.pulsateAnimation ? 1 : 0.9)
+                .animation(
+                    Animation.easeInOut(duration: 1.5)
+                        .repeatForever(autoreverses: true), value: self.pulsateAnimation
+                )
+            
+            VStack{
+                Text("Avocados".uppercased())
+                    .font(.system(size: 42, weight: .bold, design: .serif))
+                    .foregroundColor(Color.white)
+                    .padding()
+                    .shadow(color: Color("ColorBlackTransparentDark"), radius: 4, x:0, y:4)
+                Text("""
+Creamy, green, and full of nutrients!
+Avocado is a powerhouse ingredient at any meal. Enjoy these handpicked avocado recipes for breakfast, lunch, dinner & more!
+""")
+                .lineLimit(nil)
+                .font(.system(.headline, design: .serif))
+                .foregroundColor(Color("ColorGreenLight"))
+                .multilineTextAlignment(.center)
+                .lineSpacing(8)
+                .frame(maxWidth: 640, minHeight: 120)
+            }
+            .padding()
+            
+            Spacer()
+        }
+        .background(
+            Image("background")
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+        )
+        .ignoresSafeArea(.all)
+        .onAppear(perform: {
+            self.pulsateAnimation.toggle()
+        })
     }
 }
 
